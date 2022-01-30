f69d4dd4bb5848e69646dd7139f43b0cd0811350
codergalib20 <coderboygalif@gmail.com>
2022-01-30 19:31:59 +0600

    commit

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const { MongoClient } = require("mongodb");
+const ObjectId = require("mongodb").ObjectId;
+const fileUpload = require("express-fileupload");
 require("dotenv").config();
 const port = process.env.PORT || 5000;
 const app = express();
@@ -25,14 +27,16 @@ async function run() {
     await client.connect();
     const database = client.db("workManagementApplication");
     const worksCollection = database.collection("works");
+    const studentsCollection = database.collection("students");
 
+    // Here work related operation=====================
     // Create a new work____________
     app.post("/create-work", async (req, res) => {
       const work = req.body;
       const result = await worksCollection.insertOne(work);
       res.json(result);
     });
-    // Get all work____________
+    // Load all work____________
     app.get("/works", async (req, res) => {
       const cursor = worksCollection.find({});
       const works = await cursor.toArray();
@@ -42,7 +46,56 @@ async function run() {
     app.delete("/works/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: ObjectId(id) };
-      const result = await addToCartCollection.deleteOne(query);
+      const result = await worksCollection.deleteOne(query);
+      console.log(result);
+      res.json(result);
+    });
+    // Load a work___________
+    app.get("/works/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await worksCollection.findOne(query);
+      res.json(result);
+    });
+
+    // Here User related operation=========================
+    // Create a new work____________
+    app.post("/create-student", async (req, res) => {
+      const task_name = req.body.task_name;
+      const creator = req.body.creator;
+      const task_thumbnail = req.body.task_thumbnail;
+      const body = req.body.body;
+      const encodedPic = picData.toString("base64");
+      const imageBuffer = Buffer.from(encodedPic, "base64");
+      const work = {
+        task_name,
+        creator,
+        task_thumbnail,
+        body,
+        image: imageBuffer,
+      };
+      const result = await studentsCollection.insertOne(work);
+      res.json(result);
+    });
+    // Load all work____________
+    app.get("/students", async (req, res) => {
+      const cursor = studentsCollection.find({});
+      const works = await cursor.toArray();
+      res.json(works);
+    });
+    // Delete a work___________
+    app.delete("/students/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await studentsCollection.deleteOne(query);
+      console.log(result);
+      res.json(result);
+    });
+    // Load a work___________
+    app.get("/students/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await studentsCollection.findOne(query);
       res.json(result);
     });
   } finally {
